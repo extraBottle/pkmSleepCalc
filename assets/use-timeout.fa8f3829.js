67bf374bed1fac1f1843b21ad9c4448e96f6ab83
extraBottle <extraBottle@users.noreply.github.com>
2024-04-09 13:29:25 +0000

    deploy: d33eb3ff33057e433a177ff451702625601731b6

---
+++ assets/use-timeout.fa8f3829.js
@@ -0,0 +1,1 @@
+import{aa as r,b as a,ab as i,g as s}from"./index.90e0c38f.js";function l(){let e=null;const n=s();function t(){e!==null&&(clearTimeout(e),e=null)}return r(t),a(t),{removeTimeout:t,registerTimeout(o,u){t(),i(n)===!1&&(e=setTimeout(()=>{e=null,o()},u))}}}export{l as u};
