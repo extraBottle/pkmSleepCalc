aee067701eea905f1c3bed17048d0b8e972b37ea
extraBottle <128778356+extraBottle@users.noreply.github.com>
2025-06-12 09:29:43 +0900

    add maintenance

---
+++ src/pages/MaintenancePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+
+      <div class="text-h2">
+        점검중입니다! 빠르게 마칠게요...
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'MaintenancePage'
+});
+</script>
