57c3e2c8f74d8b00ed1e123217e7d20139f93d74
extraBottle <extraBottle@users.noreply.github.com>
2025-06-14 20:52:26 +0000

    deploy: 8b14c7d598576a43c1e90fc2df30241c23e938db

--- assets/gtmAddEvent.fa621321.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.7dba1d5f.js";function o(t){e().trackEvent({event:t})}export{o as t};
---
+++ assets/vue-gtm.f314493a.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index.6f3ad709.js";import{N as t}from"./index.5563b296.js";var m=r(async({app:e,router:o})=>{e.use(t({id:"GTM-PLX2WXDD",debug:!1,vueRouter:o}))});export{m as default};
