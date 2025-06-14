7910404d03e3679000d38d730ced65ede4c56e42
extraBottle <extraBottle@users.noreply.github.com>
2025-06-14 17:25:14 +0000

    deploy: 57001519eafbacb94b40a6088599a36a67cc5098

--- assets/use-quasar.96a1df8e.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ah as r}from"./index.ea306984.js";function u(){return a(r)}export{u};
---
+++ assets/vue-gtm.cb5ca4e7.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index.3fd6801a.js";import{N as t}from"./index.fa2fbc15.js";var m=r(async({app:e,router:o})=>{e.use(t({id:"GTM-PLX2WXDD",debug:!1,vueRouter:o}))});export{m as default};
