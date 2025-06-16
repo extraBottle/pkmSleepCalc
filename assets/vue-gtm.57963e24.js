630bbc17232ba618130eb49f945100577c667e3f
extraBottle <extraBottle@users.noreply.github.com>
2025-06-16 09:53:31 +0000

    deploy: 190d9926bdcfcf5b5b51490368a831079ecf2cad

---
+++ assets/vue-gtm.57963e24.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index.66613c03.js";import{N as t}from"./index.3d63358c.js";var m=r(async({app:e,router:o})=>{e.use(t({id:"GTM-PLX2WXDD",debug:!1,vueRouter:o}))});export{m as default};
