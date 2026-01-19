ae97ae726be6cd20f6eb8b76781cd0cd85334b9f
extraBottle <extraBottle@users.noreply.github.com>
2026-01-19 02:25:02 +0000

    deploy: 2ec2602e960c16b458ec0a82bb3f4ec2b0878434

---
+++ assets/gtmAddEvent.9718ac57.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.4de211c3.js";function o(t){e().trackEvent({event:t})}export{o as t};
---
+++ assets/vue-gtm.ee464720.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index.1ecb3ddd.js";import{N as t}from"./index.4de211c3.js";var m=r(async({app:e,router:o})=>{e.use(t({id:"GTM-PLX2WXDD",debug:!1,vueRouter:o}))});export{m as default};
